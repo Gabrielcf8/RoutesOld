2507edf5bd91e69d1ae0e54b54b900a2329f81aa
Gabrielcf8 <gabrielcf8@gmail.com>
2018-05-27 22:30:55 -0300

    Allowed Hots

--- WebProject/settings.py
+++ WebProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qybfgr65cw2xr#vxjyzd5hl_=b67+49taq!81fvbvkm=+5a6n='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'routeswebapp.herokuapp.com']
 
 
 # Application definition
