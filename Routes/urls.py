5239dcf66effc8c451c86e7d826fc6e4dfa5bd7e
Lucas Leme <lucas.leme.vieira@outlook.com>
2018-06-10 19:27:28 -0300

    Materialize templates

--- Routes/urls.py
+++ Routes/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^user_profile/$', views.user_profile),
     url(r'^user_form/$', views.create_profile),
     url(r'^createpoint/$', CreatePoint.as_view(), name='cadastro'),
-    url(r'^listpoint/$', ListPoint.as_view(), name='lista'),
+    url(r'^listpoint/$', ListPoint.as_view(), name='listaPoint'),
     url(r'^createcategory/$', CreateCategory.as_view(), name='cadastro'),
     url(r'^listcategory/$', ListCategory.as_view(), name='lista'),
 ]
